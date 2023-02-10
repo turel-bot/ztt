8e862ba8ceb4377e751f2d7c4c19815459f2b198
AceLikesGhosts <acelikesghosts@gmail.com>
2023-02-09 19:13:12 -0800

    FEAT: Add NotNull (stolen from apache's java validate)

---
+++ src/objects/NotNull.ts
@@ -0,0 +1,28 @@
+/**
+ * @description Checks if an Object is null, if it is throws an Error.
+ * @param {T} val - The to check if is null.  
+ * @throws
+ */
+function NotNull<T>(val: T): void
+/**
+ * @description Checks if an Object is null, if it is throws an Error.
+ * @param {T} val - The to check if is null.  
+ * @param {string} message - The message to throw the Error with. (if you'd like to provide a custom one.)
+ * @throws
+ */
+function NotNull<T>(val: T, message: string): void;
+/**
+ * @description Checks if an Object is null, if it is throws an Error.
+ * @param {T} val - The to check if is null.  
+ * @param {string?} message - The message to throw the Error with. (if you'd like to provide a custom one.)
+ * @throws
+ */
+function NotNull<T>(val: T, message?: string): void
+{
+    message = message ? message : 'Value passed to NotNull was, indeed, null.';
+
+    if(val === null)
+        throw new Error(message);
+}
+
+export default NotNull;
