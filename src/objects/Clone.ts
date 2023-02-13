f09d54d88e7ff17cf01fa1e00c28bad4e6bf35b7
AceLikesGhosts <acelikesghosts@gmail.com>
2023-02-12 23:47:28 -0800

    FEAT: Add support for Arrays in #Clone

--- src/objects/Clone.ts
+++ src/objects/Clone.ts
@@ -1,10 +1,20 @@
+import NotNull from './NotNull';
+
 /**
  * @description Clones an Object.
  * @param {T} val - The Object to clone. 
  * @returns {T}
  */
-const Clone = <T extends Record<string, unknown>>(val: T): T =>
+const Clone = <T>(val: T): T =>
 {
+    NotNull(val, 'Cannot clone an Object that is null.');
+
+    if(typeof val !== 'object')
+        return val;
+
+    if(Array.isArray(val))
+        return [...val] as T;
+
     return { ...val };
 };
 
