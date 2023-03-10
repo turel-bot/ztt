b55babac4a57e3061fb46844924050dd4a88bb00
AceLikesGhosts <acelikesghosts@gmail.com>
2023-03-10 00:35:26 -0800

    (TYPES): Allow Validate to be accessed globally

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable no-var */
 import type objectsObj from './objects';
+import type validateObj from './objects/validate';
 
 declare global
 {
-    // this is required
-    // eslint-disable-next-line no-var
     var Objects: typeof objectsObj;
+    var Validate: typeof validateObj;
 }
 
 export {  };
