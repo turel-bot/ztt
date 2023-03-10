87e1aeb3c9507a2539f2e64000677b694f1cbeca
AceLikesGhosts <acelikesghosts@gmail.com>
2023-03-10 00:15:13 -0800

    (ADD): types for Window

---
+++ src/window.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import type objectsObj from './objects';
+
+declare global
+{
+    // this is required
+    // eslint-disable-next-line no-var
+    var Objects: typeof objectsObj;
+}
+
+export { };
