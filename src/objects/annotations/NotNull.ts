d8f9cdc70fda832265e67c01858181b33d11a5f0
AceLikesGhosts <acelikesghosts@gmail.com>
2023-03-30 16:58:39 -0700

    whatever changes i staged last time

---
+++ src/objects/annotations/NotNull.ts
@@ -0,0 +1,30 @@
+/**
+ * @description 
+ * @returns {any}
+ */
+function NotNull(target: object, propertyKey: string | symbol, parameterIndex: number): any
+{
+    console.log(`target: ${ JSON.stringify(target) }`);
+    console.log(`propertyKey: ${ String(propertyKey) }`);
+    console.log(`parameterIndex: ${ parameterIndex }`);
+
+    const descriptor: Record<string, unknown> = target[propertyKey as keyof typeof target];
+    console.log((descriptor as any).call);
+
+    const targetMethod: (...args: any[]) => any = descriptor.call as any;
+
+    descriptor.call = (...args: any[]): any =>
+    {
+        for(let i: number = 0; i < args.length; i++)
+        {
+            if(i === parameterIndex)
+                if(args[parameterIndex] === null)
+                    throw new Error('Argument at index of {' + parameterIndex + '} was `null` when it was annotated by `NotNull`.');
+        }
+
+        return targetMethod(null, args);
+    };
+
+}
+
+export default NotNull;
--- src/objects/annotations/index.ts
+++ src/objects/annotations/index.ts
@@ -1,11 +1,13 @@
 import Getter from './Getter';
+import NotNull from './NotNull';
 import Setter from './Setter';
 import StaticImplements from './StaticImplement';
 
 const Annotations = {
     StaticImplements: StaticImplements,
     Getter: Getter,
-    Setter: Setter
+    Setter: Setter,
+    NotNull: NotNull
 };
 
 export default Annotations;
