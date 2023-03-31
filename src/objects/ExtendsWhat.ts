e763c3ecba42b346cbaf8073e3674d882574447e
AceLikesGhosts <acelikesghosts@gmail.com>
2023-03-30 17:16:53 -0700

    add deprecation to #ExtendsWhat due to it not working in Strict mode

--- src/objects/ExtendsWhat.ts
+++ src/objects/ExtendsWhat.ts
@@ -2,6 +2,7 @@
  * @description Gets the constructor's name, due to the way JS works this is how you get the name of the class it extends from.
  * @param clazz - The class to check.
  * @returns {string} The name of the constructor (class)
+ * @deprecated This does not work in Strict mode, please do not use this.
  */
 const ExtendsWhat = <T extends new (...args: any[]) => any>(clazz: T): string => clazz.constructor.name;
 
